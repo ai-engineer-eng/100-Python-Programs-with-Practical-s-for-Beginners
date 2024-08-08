f54543b282a3b9be1f56e18227121a57e7bc1f5a
Shafqat <wpexpert14@gmail.com>
2024-08-08 07:27:34 +0200

    updated

---
+++ 3. to find square root.py
@@ -0,0 +1,19 @@
+# #Solution 01 - (using exponentiation)
+
+# num = int(input("Enter your number: "))         #taking number from user
+
+# sr = num**(1/2)                                 #initialized a sr variable and using exponentiation method to find square root of user numberand storing to variable
+
+# print("Square root of given number is: ", sr)   #print square root of number with message.
+
+
+
+# #Solution 02 - (using math module)
+
+import math
+
+num = int(input("Enter your number: "))
+
+sr = math.sqrt(num)
+
+print("Square root of given number is: ", sr)
