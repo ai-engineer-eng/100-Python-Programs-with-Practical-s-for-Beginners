755e80fdc67826705a6025aba5f5d3844655a0fb
Shafqat <wpexpert14@gmail.com>
2024-08-08 07:10:30 +0200

    updated

--- 1. add numbers.py
+++ 1. add numbers.py
@@ -1,3 +1,12 @@
-num1 = 50
-num2 = 60
+#first method
+
+# num1 = 50
+# num2 = 60
+# print(num1 + num2)
+
+
+#second method through user input
+
+num1 = int(input("Enter first numer: "))            #asking user to input number through input method
+num2 = int(input("Enter second numer: "))
 print(num1 + num2)
