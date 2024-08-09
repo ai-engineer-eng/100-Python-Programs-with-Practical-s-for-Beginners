cd23b7ca6ed032f2ff62f919d284cbbea8a7c509
Shafqat <wpexpert14@gmail.com>
2024-08-09 08:36:49 +0200

    updated

---
+++ 4. Area of a Triangle.py
@@ -0,0 +1,9 @@
+#lets find the area of a triangle
+
+# height = float(input("Enter height of the triangle: "))         #initialized a variable to take input height of triangle
+
+# base = float(input("Enter base of the triangle: "))     #intialized a variable base
+
+# area = (1/2) * height * base            #a new variable 'area' where area will be stored, after formula
+
+# print(f"Area of Triangle is: {area}")
